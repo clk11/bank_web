9dda3943714a4f1ef7f0ea5f24caef1d703ab159
Crusoveanu Mihai <mihai_liviu2009@yahoo.com>
2023-04-06 00:37:24 +0300

    final -- v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,74 @@
 import React, { useState, useEffect } from 'react';
 import ApiFetch from './service/ApiCalls/request'
-
+import { Table } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
 import logo from './logo.svg';
 import './App.css';
+import { appendFile } from 'fs';
 
+interface DataTypeDeposit {
+  amount: number;
+  fromAddress: string;
+}
+
+interface DataTypeWithdrawls {
+  amount: number;
+  toAddress: string;
+  wasApprovedByUser2FA: boolean;
+}
+
+interface DataTypeTradeOrder {
+  amount: number;
+  tradeOrderType: {
+    name: string;
+  }
+}
+
+const columnsDeposit: ColumnsType<DataTypeDeposit> = [
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+    width: 150,
+  },
+  {
+    title: 'From Address',
+    dataIndex: 'fromAddress',
+  },
+];
+
+const columnsTradeOrders: ColumnsType<DataTypeTradeOrder> = [
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+    width: 150,
+  },
+  {
+    title: 'Trade Order Type',
+    dataIndex: ['tradeOrderType', 'name']
+  }
+];
+
+const columnsWithdrawals: ColumnsType<DataTypeWithdrawls> = [
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+    width: 150,
+  },
+  {
+    title: 'ToAddress',
+    dataIndex: 'toAddress',
+  },
+  {
+    title: '2FA Confirmed',
+    dataIndex: 'wasApprovedByUser2FA',
+    key: 'isActive',
+    render: (isActive) => (isActive ? 'True' : 'False'),
+  }
+];
+// ======================================
 function App() {
+
+  const [data, setData] = useState<any>();
   const [options, setOptions] = useState([]);
   const [selectedValue, setSelectedValue] = useState('Deposit');
 
@@ -27,11 +91,37 @@ function App() {
     fetchDropDown();
   }, []);
 
-  console.log("dp value is: ", selectedValue);
-  console.log("dp options are: ", options);
-  console.log("TEST ", process.env.REACT_APP_API_URL)
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let url = '';
+        switch (selectedValue) {
+          case 'Deposit':
+            url = ApiFetch.fetchDeposits;
+            break;
+          case 'Withdrawal':
+            url = ApiFetch.fetchWithdrawals;
+            break;
+          case 'TradeOrder':
+            url = ApiFetch.fetchTradeOrder;
+            break;
+          default:
+            url = '';
+        }
+        if (url) {
+          const response = await fetch(url);
+          const json = await response.json();
+          setData(json);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [selectedValue]);
 
+    console.log("FETCH: ", data);
+    console.log("dp value is: ", selectedValue);
   return (
     <div className="App">
       <h1>Operation table</h1>
@@ -42,6 +132,18 @@ function App() {
           </option>
         ))}
       </select>
+      <div className="Table-grid">
+      {
+      selectedValue === "Deposit" ? (
+          <Table columns={columnsDeposit} dataSource={data} pagination={{ pageSize: 5 }} scroll={{ y: 300 }} />
+			) : (
+			selectedValue === "Withdrawal" ? (
+          <Table columns={columnsWithdrawals} dataSource={data} pagination={{ pageSize: 5 }} scroll={{ y: 300 }} />
+			) : 
+      (<Table columns={columnsTradeOrders} dataSource={data} pagination={{ pageSize: 5 }} scroll={{ y: 300 }} />)
+			)
+      }
+      </div>
     </div>
   );
 }
--- src/service/ApiCalls/request.tsx
+++ src/service/ApiCalls/request.tsx
@@ -1,8 +1,8 @@
 const api_url = process.env.REACT_APP_API_URL
 const requests = {
-    fetchDeposits: api_url + '/deposit/Get',
-    fetchWithdrawals: '',
-    fetchOperationTypes: 'https://localhost:7172/OperationType/GetAll',
-    fetchTradeOrder: ''
+    fetchDeposits: 'https://localhost:7172/deposit/Get?operationTypeId=1',
+    fetchWithdrawals: 'https://localhost:7172/withdrawal/Get?operationTypeId=2',
+    fetchTradeOrder: 'https://localhost:7172/tradeorder/Get?operationTypeId=3',
+    fetchOperationTypes: 'https://localhost:7172/OperationType/GetAll'
 }
 export default requests;
