2f150bb0cf929f3f0590a13705cca8d452b96e38
Crusoveanu Mihai <mihai_liviu2009@yahoo.com>
2023-07-02 10:58:15 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import ApiFetch from './service/ApiCalls/request'
-import { Table } from 'antd';
-import type { ColumnsType } from 'antd/es/table';
-import logo from './logo.svg';
-import './App.css';
-import { appendFile } from 'fs';
+import React, { useState, useEffect } from "react";
+import ApiFetch from "./service/ApiCalls/request";
+import { Table } from "antd";
+import type { ColumnsType } from "antd/es/table";
+import logo from "./logo.svg";
+import "./App.css";
+import { appendFile } from "fs";
 
 interface DataTypeDeposit {
   amount: number;
@@ -21,69 +21,70 @@ interface DataTypeTradeOrder {
   amount: number;
   tradeOrderType: {
     name: string;
-  }
+  };
 }
 
 const columnsDeposit: ColumnsType<DataTypeDeposit> = [
   {
-    title: 'Amount',
-    dataIndex: 'amount',
+    title: "Amount",
+    dataIndex: "amount",
     width: 150,
   },
   {
-    title: 'From Address',
-    dataIndex: 'fromAddress',
+    title: "From Address",
+    dataIndex: "fromAddress",
   },
 ];
 
 const columnsTradeOrders: ColumnsType<DataTypeTradeOrder> = [
   {
-    title: 'Amount',
-    dataIndex: 'amount',
+    title: "Amount",
+    dataIndex: "amount",
     width: 150,
   },
   {
-    title: 'Trade Order Type',
-    dataIndex: ['tradeOrderType', 'name']
-  }
+    title: "Trade Order Type",
+    dataIndex: ["tradeOrderType", "name"],
+  },
 ];
 
 const columnsWithdrawals: ColumnsType<DataTypeWithdrawls> = [
   {
-    title: 'Amount',
-    dataIndex: 'amount',
+    title: "Amount",
+    dataIndex: "amount",
     width: 150,
   },
   {
-    title: 'ToAddress',
-    dataIndex: 'toAddress',
+    title: "ToAddress",
+    dataIndex: "toAddress",
   },
   {
-    title: '2FA Confirmed',
-    dataIndex: 'wasApprovedByUser2FA',
-    key: 'isActive',
-    render: (isActive) => (isActive ? 'True' : 'False'),
-  }
+    title: "2FA Confirmed",
+    dataIndex: "wasApprovedByUser2FA",
+    key: "isActive",
+    render: (isActive:any) => (isActive ? "True" : "False"),
+  },
 ];
 // ======================================
 function App() {
-
   const [data, setData] = useState<any>();
   const [options, setOptions] = useState([]);
-  const [selectedValue, setSelectedValue] = useState('Deposit');
+  const [selectedValue, setSelectedValue] = useState("Deposit");
 
-  const handleSelectDropDown = (event : any) => {
+  const handleSelectDropDown = (event: any) => {
     setSelectedValue(event.target.value);
   };
 
   useEffect(() => {
     const fetchDropDown = async () => {
       try {
-        
-          const response = await fetch(ApiFetch.fetchOperationTypes);
-          const data = await response.json();
-          const newOptions = data.map((item:any) => ({ value: item.id, label: item.name }));
-          setOptions(newOptions);
+        const response = await fetch(ApiFetch.fetchOperationTypes);
+        const data = await response.json();
+        const newOptions = data.map((item: any) => ({
+          value: item.id,
+          label: item.name,
+        }));
+        setOptions(newOptions);
       } catch (error) {
         console.error(error);
       }
@@ -94,19 +95,19 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        let url = '';
+        let url = "";
         switch (selectedValue) {
-          case 'Deposit':
+          case "Deposit":
             url = ApiFetch.fetchDeposits;
             break;
-          case 'Withdrawal':
+          case "Withdrawal":
             url = ApiFetch.fetchWithdrawals;
             break;
-          case 'TradeOrder':
+          case "TradeOrder":
             url = ApiFetch.fetchTradeOrder;
             break;
           default:
-            url = '';
+            url = "";
         }
         if (url) {
           const response = await fetch(url);
@@ -120,29 +121,41 @@ function App() {
     fetchData();
   }, [selectedValue]);
 
-    console.log("FETCH: ", data);
-    console.log("dp value is: ", selectedValue);
+  console.log("FETCH: ", data);
+  console.log("dp value is: ", selectedValue);
   return (
     <div className="App">
       <h1>Operation table</h1>
       <select value={selectedValue} onChange={handleSelectDropDown}>
-        {options.map((option:any) => (
+        {options.map((option: any) => (
           <option key={option.value} value={option.label}>
             {option.label}
           </option>
         ))}
       </select>
       <div className="Table-grid">
-      {
-      selectedValue === "Deposit" ? (
-          <Table columns={columnsDeposit} dataSource={data} pagination={{ pageSize: 5 }} scroll={{ y: 300 }} />
-			) : (
-			selectedValue === "Withdrawal" ? (
-          <Table columns={columnsWithdrawals} dataSource={data} pagination={{ pageSize: 5 }} scroll={{ y: 300 }} />
-			) : 
-      (<Table columns={columnsTradeOrders} dataSource={data} pagination={{ pageSize: 5 }} scroll={{ y: 300 }} />)
-			)
-      }
+        {selectedValue === "Deposit" ? (
+          <Table
+            columns={columnsDeposit}
+            dataSource={data}
+            pagination={{ pageSize: 5 }}
+            scroll={{ y: 300 }}
+          />
+        ) : selectedValue === "Withdrawal" ? (
+          <Table
+            columns={columnsWithdrawals}
+            dataSource={data}
+            pagination={{ pageSize: 5 }}
+            scroll={{ y: 300 }}
+          />
+        ) : (
+          <Table
+            columns={columnsTradeOrders}
+            dataSource={data}
+            pagination={{ pageSize: 5 }}
+            scroll={{ y: 300 }}
+          />
+        )}
       </div>
     </div>
   );
